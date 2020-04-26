a3c8839d5e28d8a78c8b394a92c7f31d78f2fc5f
Rich Lander <rlander@microsoft.com>
2020-04-26 15:45:34 -0700

    Fix off-by-one error in Dim function

--- samples/led-bar-graph/AnimateLeds.cs
+++ samples/led-bar-graph/AnimateLeds.cs
@@ -8,6 +8,8 @@ public class AnimateLeds
 {
     private CancellationToken _cancellation;
     private IPinSegment _pinSegment;
+    private int[] _pins;
+    private int[] _pinsReverse;
     public int LitTimeDefault = 200;
     public int DimTimeDefault = 50;
     public int LitTime = 200;
@@ -17,6 +19,8 @@ public class AnimateLeds
     {        
         _pinSegment = pinSegment;
         _cancellation = token;
+        _pins = Enumerable.Range(0,_pinSegment.Length).ToArray();
+        _pinsReverse = _pins.Reverse().ToArray();
     }
 
     private void CycleLeds(params int[] pins)
@@ -60,14 +64,12 @@ public class AnimateLeds
     {
         Console.WriteLine(nameof(FrontToBack));
         var iterations = skipLast ? _pinSegment.Length : _pinSegment.Length - 2;
-        Sequence(Enumerable.Range(0,iterations));
-        //Sequence(_pinSegment.ToArray().AsSpan(0,iterations).ToArray());
+        Sequence(_pins.AsSpan(0,iterations).ToArray());
     }
     public void BacktoFront()
     {
         Console.WriteLine(nameof(BacktoFront));
-        Sequence(Enumerable.Range(0,_pinSegment.Length).Reverse());
-        //Sequence(_pinSegment.Reverse());
+        Sequence(_pinsReverse);
     }
 
     public void MidToEnd()
@@ -78,7 +80,6 @@ public class AnimateLeds
         if (_pinSegment.Length % 2 == 1)
         {
             CycleLeds(half);
-            //CycleLeds(_pinSegment[half]);
         }
 
         for (var i = 1; i < half+1; i ++)
@@ -87,7 +88,6 @@ public class AnimateLeds
             var pinB = half - 1 + i;
 
             CycleLeds(pinA,pinB);
-            //CycleLeds(_pinSegment[ledA],_pinSegment[ledB]);
         }
     }
 
@@ -102,26 +102,23 @@ public class AnimateLeds
             var ledB = _pinSegment.Length - 1 - i;
 
             CycleLeds(ledA, ledB);
-            //CycleLeds(_pinSegment[ledA], _pinSegment[ledB]);
         }
 
         if (_pinSegment.Length % 2 == 1)
         {
             CycleLeds(half);
-            //CycleLeds(_pinSegment[half]);
         }
     }
 
     public void LightAll()
     {
         Console.WriteLine(nameof(LightAll));
-        foreach(var pin in Enumerable.Range(0,_pinSegment.Length))
+        foreach(var pin in _pins)
         {
             if (_cancellation.IsCancellationRequested)
             {
                 return;
             }
-
             _pinSegment.Write(pin, 1);
         }
         Thread.Sleep(LitTime);
@@ -132,7 +129,7 @@ public class AnimateLeds
         Console.WriteLine(nameof(DimAllAtRandom));
         var random = new Random();
 
-        var pinList = Enumerable.Range(0,_pinSegment.Length).ToList();
+        var pinList = _pins.ToList();
 
         while (pinList.Count > 0)
         {
@@ -141,7 +138,7 @@ public class AnimateLeds
                 return;
             }
 
-            var pin = random.Next(_pinSegment.Length-1);
+            var pin = random.Next(_pinSegment.Length);
 
             if (pinList.Remove(pin))
             {
